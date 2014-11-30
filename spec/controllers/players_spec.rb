8083367809fefe6d5b2c7863bd3808ecfa40b819
Luke Davitt <davittluke@gmail.com>
2014-11-30 14:45:16 -0500

    slight mod to 404 test

--- spec/controllers/players_spec.rb
+++ spec/controllers/players_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe PlayersController, :type => :controller do
 	  player = Player.create(first_name:"something", last_name:"else", score: 0)
 	  player.id = 20000
       get :show, id: player
-      expect(response).to render_template(:file => "#{Rails.root}/public/404.html")
+      expect(response.status).to eq(404)
     end
   end
 
