9be69ad6f3788131f4307faa34feb18bc12b2dd6
Luke Davitt <davittluke@gmail.com>
2014-12-02 01:15:44 -0500

    stupid stupid stupid

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,14 @@ class Player < ActiveRecord::Base
 	validates :last_name, presence: true
 	validates :score, :numericality => {:only_integer => true}
 	
+	def show_rank
+		records = Player.descending.all
+		rank = nil 
+		records.each_with_index do |player, index| 			
+			if self.id == player.id
+				rank = index + 1
+			end
+		end
+		rank
+	end
 end
